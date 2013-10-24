af67c0d13daefd4053a873def813111b4b7ffb1a
William Fischer <billfischer510@gmail.com>
2013-10-23 23:12:45 -0400

    Added Scraper, changes to elo.py
    Elo.py now calculates the ratings for every game in the set
    (all data from 2001 onwards takes less than a second to process)
    Teams are added dynamically
    Loading the csv added by the scraper
    Scraper will grab slowly all the data it can from nfl.com and create a
    csv file for use in the elo creator.

--- elo.py
+++ elo.py
@@ -1,3 +1,15 @@
+csvFile = open('data.csv', 'r')
+results = open('results.txt', 'w')
+teamRatingsFile = open('ratings.txt', 'w')
+games = []
+
+#bounds
+earliestYear = 2010
+latestYear = 2013
+postSeasonOnly = False
+preSeasonOnly = True
+
+
 def computeEstimatedResult(team1, team2):
 	exponent = (team2 - team1)/400
 	value = 1/(1 + (10 ** exponent))
@@ -11,50 +23,103 @@ def computeNewRatings(team1, predictedScore, actualScore, isPreseason):
 	value = team1 + (skillFactor * (actualScore - predictedScore))
 	return value
 
+def loadCSV():
+	header = csvFile.readline()
+	for line in csvFile:
+		(homeTeam,homeFirstQuater,homeSecondQuater,homeThirdQuater,
+			homeFourthQuater,homeOTQuater,homeTotalScore,awayTeam,
+			awayFirstQuater,awaySecondQuater,awayThirdQuater,
+			awayFourthQuater,awayOTQuater,awayTotalScore,date, preseason, postseason) = line.split(',')
+		awayBoxScore = []
+		homeBoxScore = []
+
+		awayBoxScore.append(int(awayFirstQuater))
+		awayBoxScore.append(int(awaySecondQuater))
+		awayBoxScore.append(int(awayThirdQuater))
+		awayBoxScore.append(int(awayFourthQuater))
+		awayBoxScore.append(int(awayOTQuater))
+
+		homeBoxScore.append(int(homeFirstQuater))
+		homeBoxScore.append(int(homeSecondQuater))
+		homeBoxScore.append(int(homeThirdQuater))
+		homeBoxScore.append(int(homeFourthQuater))
+		homeBoxScore.append(int(homeOTQuater))
+
+		games.append(Game(awayTeam, homeTeam, awayBoxScore, homeBoxScore, int(awayTotalScore), int(homeTotalScore), False, False, date))
+	csvFile.close()
+
 def initData():
-	#teams
-	teams = ['Bears','Cardinals','Packers','Giants','Lions','Redskins','Steelers','Eagles','Rams','49ers','Browns','Colts','Cowboys','Raiders','Patriots','Titans','Broncos','Chargers','Jets','Chiefs','Bills','Vikings','Dolphins','Falcons','Saints','Bengals','Seahawks','Buccaneers','Jaguars','Panthers','Ravens','Texans']
-	
-	#use 1500 as our base rating
-	for team in teams:
-		teamRating[team] = 1500
-
-	#2013 preseason week 1
-
-	games = []
-	games.append(Game('Ravens', 'Buccaneers', [0, 24, 7, 13], [3,10,3,0], True))
+	loadCSV()
 
 	#compute ratings
 	for game in games:
 		#add a check that a game is in the key set of the ratings maps
-		for i in range(0, game.numQuaters()):
-			print("\nRating for the " + game.homeTeam + " is: " + str(teamRating[game.homeTeam]))
-			print("Rating for the " + game.awayTeam + " is: " + str(teamRating[game.awayTeam]))
-			eSHome = computeEstimatedResult(teamRating[game.homeTeam], teamRating[game.awayTeam])
-			eSAway = computeEstimatedResult(teamRating[game.awayTeam], teamRating[game.homeTeam])
-			teamRating[game.homeTeam] = computeNewRatings(teamRating[game.homeTeam], eSHome, (game.homeScore[i]/(game.awayScore[i] + game.homeScore[i])), game.isPreseason)
-			teamRating[game.awayTeam] = computeNewRatings(teamRating[game.awayTeam], eSAway, (game.awayScore[i]/(game.awayScore[i] + game.homeScore[i])), game.isPreseason)
-			print("\nNew Rating for the " + game.homeTeam + " is: " + str(teamRating[game.homeTeam]))
-			print("New Rating for the " + game.awayTeam + " is: " + str(teamRating[game.awayTeam]))
-			
+		skip = False
+		if(game.homeTeam not in teamRating):
+			teamRating[game.homeTeam] = 1500
+		if(game.awayTeam not in teamRating):
+			teamRating[game.awayTeam] = 1500
+		for i in range(0, 5):
+			skip = False
+			if(i == 4):
+				if(game.homeScore == 0):
+					if(game.awayScore == 0):
+						skip = True
+			if(not skip):
+				eSHome = computeEstimatedResult(teamRating[game.homeTeam], teamRating[game.awayTeam])
+				eSAway = computeEstimatedResult(teamRating[game.awayTeam], teamRating[game.homeTeam])
+				scoreHome = 0
+				scoreAway = 0
+				if((game.awayBoxScore[i] + game.homeBoxScore[i]) != 0):
+					scoreHome = (game.homeBoxScore[i]/(game.awayBoxScore[i] + game.homeBoxScore[i]))
+					scoreAway = (game.awayBoxScore[i]/(game.awayBoxScore[i] + game.homeBoxScore[i]))
+				else:
+					scoreHome = 0.5
+					scoreAway = 0.5
+				teamRating[game.homeTeam] = computeNewRatings(teamRating[game.homeTeam], eSHome, scoreHome, game.isPreseason)
+				teamRating[game.awayTeam] = computeNewRatings(teamRating[game.awayTeam], eSAway, scoreAway, game.isPreseason)
 
+				results.write('\n')
+				results.write('Home team: ' + game.homeTeam + '\n')
+				results.write('Away team: ' + game.awayTeam + '\n')
+				results.write('eSHome: ' + str(eSHome) + '\n')
+				results.write('eSAway: ' + str(eSAway) + '\n')
+				results.write('scoreHome: ' + str(scoreHome) + '\n')
+				results.write('scoreAway: ' + str(scoreAway) + '\n')
+				results.write('Home Rating: ' + str(teamRating[game.homeTeam]) + '\n')
+				results.write('Away Rating: ' + str(teamRating[game.awayTeam]) + '\n')
+
+	for team in teamRating:
+		print('Team: ' + team)
+		print('\tRating: ' + str(teamRating[team]))
+
+	for team in teamRating:
+		teamRatingsFile.write('\nTeam: ' + team + '\n')
+		teamRatingsFile.write('\tRating: ' + str(teamRating[team]) + '\n')
 
 
 
 
 class Game:
-	def __init__(self, awayTeam, homeTeam, awayScore, homeScore, preseason):
+	def __init__(self, awayTeam, homeTeam, awayBoxScore, homeBoxScore, awayScore, homeScore, preseason, postseason, date):
 		self.awayTeam = awayTeam
 		self.homeTeam = homeTeam
 		self.awayScore = awayScore
 		self.homeScore = homeScore
 		self.preseason = preseason
+		self.postseason = postseason
+		self.homeBoxScore = homeBoxScore
+		self.awayBoxScore = awayBoxScore
+		self.date = date
 
 	def numQuaters(self):
 		return len(self.awayScore)
 
 	def isPreseason(self):
 		return self.preseason
+
+	def isPostseason(self):
+		return self.postseason
 
 
 teamRating = {}
---
+++ testScrape.py
@@ -0,0 +1,181 @@
+import urllib.request
+import time
+
+urlPrefix = 'http://www.nfl.com/scores/'
+scoreBoxString = '<div class="new-score-box">'
+quatersString = '<p class="quarters-score">'
+go = True
+dataFile = open('data.csv', 'w')
+
+def getURL(year, week, preseason, postseason):
+	url = urlPrefix + str(year) + '/'
+	if(preseason):
+		url += 'PRE' + str(week)
+	else:
+		if(postseason):
+			url += 'POST' + str(week)
+		else:
+			url += 'REG' + str(week)
+	return url
+
+def getPage(url):
+	return urllib.request.urlopen(url).read().decode('utf-8')
+
+def trimPage(source):
+	start = source.index(scoreBoxString)
+	source = source[start:]
+	return source
+
+#only does one game per page so far
+def getNextGameIndex(source):
+	games = []
+	while source.find(scoreBoxString) != -1:
+		start = source.index(scoreBoxString)
+		end = source[start + len(scoreBoxString):]
+		if(end.find(scoreBoxString) != -1):
+			end = end.index(scoreBoxString)
+			game = source[start:end]
+			games.append(game)
+			source = source[end:]
+		else:
+			game = source[start:]
+			games.append(game)
+			source = ''
+	return games
+	
+
+def parseGame(gameSource):
+	#init
+	awayTeam = gameSource
+	awayTotalScore = 0
+	awayBoxScore = []
+
+	#parse away team name
+	awayTeam = gameSource[gameSource.index("team-name"):]
+	awayTeam = awayTeam[:awayTeam.index("</a></p>")]
+	awayTeam = awayTeam[awayTeam.rindex(">") + 1:]
+	
+	#parse away total score
+	awayTotalScore = gameSource[gameSource.index('total-score">') + 13:]
+	awayTotalScore = awayTotalScore[:awayTotalScore.index('</p>')]
+	
+	#parse box score
+	gameSource = gameSource[gameSource.index(quatersString):]
+	awayBoxScoreSource = gameSource[:gameSource.index('</p>')]
+	first = awayBoxScoreSource[awayBoxScoreSource.index('<span class="first-qt">'):awayBoxScoreSource.index('</span>')]
+	first = first[first.index('>') + 1 :]
+	awayBoxScoreSource = awayBoxScoreSource[awayBoxScoreSource.index('<span class="second-qt">'):]
+	second = awayBoxScoreSource[awayBoxScoreSource.index('<span class="second-qt">'):awayBoxScoreSource.index('</span>')]
+	second = second[second.index('>') + 1 :]
+	awayBoxScoreSource = awayBoxScoreSource[awayBoxScoreSource.index('<span class="third-qt">'):]
+	third = awayBoxScoreSource[awayBoxScoreSource.index('<span class="third-qt">'):awayBoxScoreSource.index('</span>')]
+	third = third[third.index('>') + 1 :]
+	awayBoxScoreSource = awayBoxScoreSource[awayBoxScoreSource.index('<span class="fourth-qt">'):]
+	fourth = awayBoxScoreSource[awayBoxScoreSource.index('<span class="fourth-qt">'):awayBoxScoreSource.index('</span>')]
+	fourth = fourth[fourth.index('>') + 1 :]
+	awayBoxScoreSource = awayBoxScoreSource[awayBoxScoreSource.index('<span class="ot-qt">'):]
+	ot = awayBoxScoreSource[awayBoxScoreSource.index('<span class="ot-qt">'):awayBoxScoreSource.index('</span>')]
+	ot = ot[ot.index('>') + 1 :]
+	awayBoxScore.append(first)
+	awayBoxScore.append(second)
+	awayBoxScore.append(third)
+	awayBoxScore.append(fourth)
+	if(len(ot) != 0):
+		awayBoxScore.append(ot)
+	else:
+		awayBoxScore.append('0')
+	
+	#will do this the ugly way for now and just copy code
+
+	gameSource = gameSource[gameSource.index("team-name"):]
+
+	#init
+	homeTotalScore = 0
+	homeBoxScore = []
+
+	#parse home team name
+	homeTeam = gameSource[gameSource.index("team-name"):]
+	homeTeam = homeTeam[:homeTeam.index("</a></p>")]
+	homeTeam = homeTeam[homeTeam.rindex(">") + 1:]
+	
+	#parse home total score
+	homeTotalScore = gameSource[gameSource.index('total-score">') + 13:]
+	homeTotalScore = homeTotalScore[:homeTotalScore.index('</p>')]
+	
+	#parse box score
+	gameSource = gameSource[gameSource.index(quatersString):]
+	gameSource = gameSource[:gameSource.index('</p>')]
+	first = gameSource[gameSource.index('<span class="first-qt">'):gameSource.index('</span>')]
+	first = first[first.index('>') + 1 :]
+	gameSource = gameSource[gameSource.index('<span class="second-qt">'):]
+	second = gameSource[gameSource.index('<span class="second-qt">'):gameSource.index('</span>')]
+	second = second[second.index('>') + 1 :]
+	gameSource = gameSource[gameSource.index('<span class="third-qt">'):]
+	third = gameSource[gameSource.index('<span class="third-qt">'):gameSource.index('</span>')]
+	third = third[third.index('>') + 1 :]
+	gameSource = gameSource[gameSource.index('<span class="fourth-qt">'):]
+	fourth = gameSource[gameSource.index('<span class="fourth-qt">'):gameSource.index('</span>')]
+	fourth = fourth[fourth.index('>') + 1 :]
+	gameSource = gameSource[gameSource.index('<span class="ot-qt">'):]
+	ot = gameSource[gameSource.index('<span class="ot-qt">'):gameSource.index('</span>')]
+	ot = ot[ot.index('>') + 1 :]
+	homeBoxScore.append(first)
+	homeBoxScore.append(second)
+	homeBoxScore.append(third)
+	homeBoxScore.append(fourth)
+	if(len(ot) != 0):
+		homeBoxScore.append(ot)
+	else:
+		homeBoxScore.append('0')
+
+	return (homeTeam,homeBoxScore,homeTotalScore,awayTeam,awayBoxScore,awayTotalScore)
+
+def parseSteps(year, week, preseason, postseason):
+	url = getURL(year, week, preseason, postseason)
+	f = getPage(url)
+	trimedPage = trimPage(f)
+	games = getNextGameIndex(trimedPage)
+	for game in games:
+		(homeTeam,homeBoxScore,homeTotalScore,awayTeam,awayBoxScore,awayTotalScore) = parseGame(game)
+		print(homeTeam + ": " + homeTotalScore + "\n" + awayTeam + ": " + awayTotalScore)
+		homeBoxScoreString = ''
+		awayBoxScoreString = ''
+		for i in homeBoxScore:
+			homeBoxScoreString += ',' + i
+		for i in awayBoxScore:
+			awayBoxScoreString += ',' + i
+
+		dataFile.write(homeTeam +homeBoxScoreString+ ',' +homeTotalScore+ ',' +awayTeam+ awayBoxScoreString+ ',' +awayTotalScore + ',' + str(week) + ':' + str(year) +':' + str(preseason) + ':' + str(postseason) + ',' + str(preseason) + ',' + str(postseason) +'\n')
+	print(str(year) + ' ' + str(week) + ' ' + str(preseason) + ' ' + str(postseason))
+	#sleep so we can keep doing this without nfl thinking we're assholes / hackers
+	time.sleep(2)
+
+dataFile.write('homeTeam,homeFirstQuater,homeSecondQuater,homeThirdQuater,homeFourthQuater,homeOTQuater,homeTotalScore,awayTeam,awayFirstQuater,awaySecondQuater,awayThirdQuater,awayFourthQuater,awayOTQuater,awayTotalScore,date,preseason,postseason\n')
+
+if(go):
+	for year in range(2001, 2012):
+		for week in range(1,5):
+			parseSteps(year, week, True, False)
+		for week in range(1,18):
+			parseSteps(year, week, False, False)
+		parseSteps(year, 18, False, True)
+		parseSteps(year, 19, False, True)
+		parseSteps(year, 20, False, True)
+		if(year <= 2008):
+			parseSteps(year, 21, False, True)
+		else:
+			parseSteps(year, 22, False, True)
+	for week in range(1,4):
+		parseSteps(2013, week, True, False)
+	for week in range(1,8):
+		parseSteps(2013, week, False, False)
+else:
+	parseSteps(2013, 1, False, False)
+	parseSteps(2013, 2, False, False)
+
+
+
+
+
+
+
